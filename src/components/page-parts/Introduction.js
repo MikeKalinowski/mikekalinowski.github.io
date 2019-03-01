eb407e73dc6151adacb260fe87e63809af3eaadc
MikeKalinowski <MikeKalinowski@users.noreply.github.com>
2019-03-01 12:40:39 +0100

    first commit

---
+++ src/components/page-parts/Introduction.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import styled from 'styled-components'
+
+const IntroductionWrapper = styled.div`
+`
+
+class Introduction extends Component {
+  render() {
+    return (
+      <IntroductionWrapper>
+      </IntroductionWrapper>
+    );
+  }
+}
+
+export default Introduction;
---
+++ src/components/page-parts/Projects.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import styled from 'styled-components'
+
+const ProjectsWrapper = styled.div`
+`
+
+class Projects extends Component {
+  render() {
+    return (
+      <ProjectsWrapper>
+      </ProjectsWrapper>
+    );
+  }
+}
+
+export default Projects;
