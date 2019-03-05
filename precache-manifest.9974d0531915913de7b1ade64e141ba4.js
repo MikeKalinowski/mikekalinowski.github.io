297914a0feeeedc3564c6fbc7959b84ae0fba5f8
MikeKalinowski <MikeKalinowski@users.noreply.github.com>
2019-03-05 13:45:29 +0100

    Updates

---
+++ precache-manifest.9974d0531915913de7b1ade64e141ba4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc05f3cdab8abf4385a107b9691f120e",
+    "url": "/static/media/rpgSmall.dc05f3cd.png"
+  },
+  {
+    "revision": "3dca4328a59b708a3a35ba8354f054c9",
+    "url": "/static/media/gatsbySmall.3dca4328.png"
+  },
+  {
+    "revision": "d9824e356dc13cee9d83f1c2c8e1380d",
+    "url": "/static/media/background.d9824e35.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "11e20c67841ef2803e14",
+    "url": "/static/js/main.11e20c67.chunk.js"
+  },
+  {
+    "revision": "3335b7b9465365d5a5af",
+    "url": "/static/js/2.3335b7b9.chunk.js"
+  },
+  {
+    "revision": "11e20c67841ef2803e14",
+    "url": "/static/css/main.25d0c9b6.chunk.css"
+  },
+  {
+    "revision": "872d445b0e9b02c559a2a8e34aeab0bb",
+    "url": "/index.html"
+  }
+];
